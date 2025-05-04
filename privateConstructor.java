4840f38b739cc5e0166910972eac4796d25f0611
Hrushiprada Swain <hrushipradaswain@gmail.com>
2025-05-04 18:14:17 +0530

    Add files via upload
    Java program uses a private constructor

---
+++ privateConstructor.java
@@ -0,0 +1,20 @@
+class He {
+    int a;
+    String b;
+    double c;
+    private He(){
+        a=45;
+        b="Harry";
+        c=78.875;
+        System.out.print(a+" "+b+" "+c);
+    }
+    public static void main(String[] args){
+        He n1=new He();
+        }
+}
+/*class B6{
+    public static void main(String[] args){
+        He r1=new He();
+        //He r2=new He(r1);
+    }
+}*/
