140a6e6dd1efe9c830e7593061a68f2fc719bd70
Hrushiprada Swain <hrushipradaswain@gmail.com>
2025-05-13 21:10:32 +0530

    Add files via upload

---
+++ IntializeObj.java
@@ -0,0 +1,10 @@
+class Animalproj{
+    String color;
+    int age;
+    public static void main(String[] args){
+        Animalpro burzo=new Animalpro();
+        burzo.color="black";
+        burzo.age=10;
+        System.out.print(burzo.color+" "+burzo.age);
+    }
+}
