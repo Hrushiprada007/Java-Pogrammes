8c8475d9dd84485add4c2a471fc9409a1d53141c
Hrushiprada Swain <hrushipradaswain@gmail.com>
2025-11-03 23:07:18 +0530

    Add files via upload

---
+++ arithmeticException.java
@@ -0,0 +1,12 @@
+class arithmeticException 
+{
+	public static void main(String[] args) 
+	{
+		int a = 20;
+		double b =0.0;
+		System.out.println("a value:"+a);
+		System.out.println("b value:"+b);
+		double c = a/b;
+		System.out.println("c value:"+c);
+	}
+}
